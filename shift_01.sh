4cfd111bea7874aa0b06025263e7d2a01c7a42f6
omottec <omottec@gmail.com>
2020-06-03 13:07:57 +0800

    -zsh, , , 0

---
+++ shift_01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "This script's name is:$0"
+echo "The first parameter is:$1"
+echo "The Second parameter is:$2"
+echo "The Third parameter is:$3"
+echo "All of the parameters are $@"
+echo "Count of parameter is:$#"
