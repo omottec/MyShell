0429fa8dcb5d2d562d09fff355d849b10820d2ae
omottec <omottec@gmail.com>
2020-07-27 21:54:08 +0800

    until

---
+++ until01.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sum01=0
+sum02=0
+i=1
+until [[ $i -gt 100 ]]
+do
+    let "sum01+=i"
+    let "j=i%2"
+    if [[ $j -ne 0 ]]; then
+        let "sum02+=i"
+    fi
+    let "i+=1"
+done
+echo "$sum01"
+echo "$sum02"
